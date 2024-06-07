35c6922a7a3d7fa5b78f692ef85001b2980bd758
Antonio <acgomesn@gmail.com>
2024-06-06 22:34:14 -0300

    Use Case and Design Patterns - Repository Interface

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
@@ -8,16 +9,12 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-    constructor(private usersRepository: any) { }
+    constructor(private usersRepository: UsersRepository) { }
 
     async execute({ name, email, password }: RegisterUseCaseRequest) {
         const password_hash = await hash(password, 6);
 
-        const userWIthSameEmail = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        });
+        const userWIthSameEmail = await this.usersRepository.findByEmail(email);
 
         if (userWIthSameEmail) {
             throw new Error('Email already exists')
