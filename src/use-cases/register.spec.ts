1a6e8b71c29ee757029d20a74cedf825fcd75dc4
Antonio <acgomesn@gmail.com>
2024-06-14 22:59:48 -0300

    fix - error test of password hash

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -25,7 +25,7 @@ describe('Register Use Case', () => {
         const { user } = await registerUseCase.execute({
             name: 'John Doe',
             email: 'john@doe.com',
-            password: '1234567'
+            password: '123456'
         })
 
         const isPasswordCorrectlyHashed = await compare('123456', user.password_hash)
