e2951c05e15a22b21039856e6510f059e80ee9dd
Antonio <acgomesn@gmail.com>
2024-10-05 20:04:41 -0300

    creating test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,16 @@
+import { Environment } from 'vitest'
+
+export default <Environment>{
+    name: 'prisma',
+    transformMode: 'ssr',
+    async setup() {
+        console.log('setup')
+
+        return {
+            teardown() {
+                console.log('teardown')
+
+            }
+        }
+    },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
     plugins: [
         tsconfigPaths()
-    ]
+    ],
+    test: {
+        environmentMatchGlobs: [
+            ['src/http/controllers/**', 'prisma']
+        ],
+        dir: 'src',
+    }
 })
